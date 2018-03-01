72987f448f878362a4d557aaf6a10889282759b9
Marina <schwmk@gmail.com>
2018-03-01 14:03:21 +0300

    79 passing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  var i = 0;
-  var d = 5;
-  if (base > 16) {
-    d = Math.round(base / 2);
-  }
+  var i = 0; 
   while (number > 0) {
-    number = Math.floor(number / d);
+    number = Math.floor(number / getLargestPrime(base));
     i = i + number;
   }
   return i;
 }
+
+function getLargestPrime(base) {
+  var arrayOfPrimes = [];
+  var largestPrime;
+  for (let i = 2; i <= base; i++) {
+      while ((base % i) === 0) {
+        arrayOfPrimes.push(i);
+        base /= i;
+      }
+      arrayOfPrimes.sort(compareNumbers);
+      largestPrime = arrayOfPrimes[0];      
+  }
+  return largestPrime;
+}
+
+function compareNumbers(a, b) {
+  if (a > b) return -1;
+  if (a < b) return 1;
+}
+
